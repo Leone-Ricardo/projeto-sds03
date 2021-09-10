582e64c977fb21ea723c42d0a6cb1de111c7a405
Leone Ricardo <leoneonix688@gmail.com>
2021-09-09 21:54:36 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/desvendas/repositories/SaleRepository2.java
@@ -0,0 +1,9 @@
+package com.devsuperior.desvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.desvendas.entities.Sale;
+
+public interface SaleRepository2 extends JpaRepository <Sale, Long>{
+
+}
